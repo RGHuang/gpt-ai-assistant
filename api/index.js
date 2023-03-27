98df9ce717b70a04e5493ebb74bb21237e31acc7
roger860512 <ziwei443@gmail.com>
2023-03-27 17:43:50 +0800

    fix: internal error

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
-import express from "express";
-import { isNil, get } from "lodash";
+import express from 'express';
 
-import { handleEvents, printPrompts, recordEvent } from "../app/index.js";
-import config from "../config/index.js";
-import { validateLineSignature } from "../middleware/index.js";
+import { handleEvents, printPrompts, recordEvent } from '../app/index.js';
+import config from '../config/index.js';
+import { validateLineSignature } from '../middleware/index.js';
 
 const app = express();
 
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  })
-);
+app.use(express.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf.toString();
+  },
+}));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   if (config.APP_URL) {
     res.redirect(config.APP_URL);
     return;
@@ -25,9 +22,7 @@ app.get("/", (req, res) => {
 
 app.post(config.APP_WEBHOOK_PATH, validateLineSignature, async (req, res) => {
   try {
-    if (!isNil(get(req.body.events, "0.source.userId"))) {
-      await recordEvent(req.body.events);
-    }
+    await recordEvent(req.body.events);
     await handleEvents(req.body.events);
     res.sendStatus(200);
   } catch (err) {
--- app/record-event.js
+++ app/record-event.js
@@ -1,11 +1,12 @@
+import { get } from 'lodash';
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../services/firebase.js';
 
 const recordEvent = async (events = []) => {
   const lineContext = {
-    userId: events[0].source.userId,
+    userId: get(events[0], 'source.userId'),
     time: Date.now(),
-    message: events[0].message.text,
+    message: get(events[0], 'message.text'),
   };
   console.log(lineContext);
 
